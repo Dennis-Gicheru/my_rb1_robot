c32e6c1d1be3acbba9a957cd69ed31daedbc740b
Dennis Gicheru <dennisgicheru254@gmail.com>
2025-10-22 16:10:27 +0000

    Task 3: Add Sensor Plugins

--- my_rb1_description/setup.py
+++ my_rb1_description/setup.py
@@ -1,25 +1,33 @@
-from setuptools import find_packages, setup
+import os
+from glob import glob
+from setuptools import setup
+from setuptools import find_packages
+
 
 package_name = 'my_rb1_description'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(exclude=['tests', 'tests.*']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.*')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='user',
+    maintainer='Dennis_Gicheru',
     maintainer_email='dennisgicheru254@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='my cylinder robot',
+    license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
+         'console_scripts': [
         ],
     },
 )
